38012c532107f1bec0979089d49a6208844b6326
jcolandr <jcolandr@gmail.com>
2023-11-02 12:41:19 -0400

    subnet-id

--- mongo.tf
+++ mongo.tf
@@ -9,10 +9,9 @@ variable "ami_id" {
 }
 
 provider "aws" {
-  region = "us-east-2" # Change this to your desired AWS region
-}
+  region = "us-east-2" 
 
-resource "aws_instance" "example" {
+resource "aws_instance" "mongo" {
   ami           = var.ami_id
   instance_type = "t2.micro"
   key_name      = "jdc-aws" 
@@ -35,7 +34,7 @@ resource "aws_instance" "example" {
 }
 
 # Example security group to allow SSH and MongoDB access
-resource "aws_security_group" "example" {
+resource "aws_security_group" "mongo-sg" {
   name        = "example-security-group"
   description = "Allow SSH and MongoDB access"
 
@@ -66,12 +65,12 @@ resource "aws_security_group" "example" {
 }
 
 # Associate the security group with the EC2 instance
-resource "aws_network_interface_sg_attachment" "example" {
-  security_group_id    = aws_security_group.example.id
-  network_interface_id = aws_instance.example.network_interface_ids[0]
+resource "aws_network_interface_sg_attachment" "mongo-sg" {
+  security_group_id    = aws_security_group.mongo-sg.id
+  network_interface_id = aws_instance.mongo.primary_network_interface_id
 }
 
 output "instance_public_ip" {
-  value = aws_instance.example.public_ip
+  value = aws_instance.mongo.public_ip
 }
 
