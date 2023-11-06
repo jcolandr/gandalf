4e4d8aadf9f8a6ce5d064c8da89dc5a22b7c6a55
jcolandr <jcolandr@gmail.com>
2023-11-06 15:02:17 -0500

    web deployment

---
+++ web.tf
@@ -0,0 +1,72 @@
+resource "kubernetes_deployment" "node" {
+  metadata {
+    name = "scalable-node-example"
+    labels = {
+      App = "ScalablenodeExample"
+    }
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "ScalablenodeExample"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "ScalablenodeExample"
+        }
+      }
+      spec {
+        container {
+          image = "ijason/nodejs-sample:latest"
+          name  = "web"
+
+          port {
+            container_port = 3000
+          }
+
+          env {
+            name = "MONGO_HOST"
+            value = "frodo:baggins@3.145.34.154"
+          }
+          
+          env {
+            name = "MONGO_PORT"
+            value = "27017"
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "node" {
+  metadata {
+    name = "node-example"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.node.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      port        = 80
+      target_port = 3000
+    }
+
+    type = "LoadBalancer"
+  }
+}
